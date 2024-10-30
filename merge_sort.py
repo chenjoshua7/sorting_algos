86a625f29b059d09368a2c94bea0e13e9fcae61e
Joshua Chen <chen.joshua98@gmail.com>
2024-10-30 14:51:03 -0400

    finished merge_sort

--- merge_sort.py
+++ merge_sort.py
@@ -5,7 +5,7 @@ def merge_sort(nums: List[int]) -> List[int]:
     n = len(nums_)
     
     first_half = nums_[:n//2]
-    second_half = nums_[n//2+1:]
+    second_half = nums_[n//2:]
 
     if len(first_half) > 1:
         first_half = merge_sort(first_half)
@@ -16,7 +16,7 @@ def merge_sort(nums: List[int]) -> List[int]:
     sorted_list = []
     
     while first_half and second_half:
-        if first_half[0] >= second_half[0]:
+        if first_half[0] <= second_half[0]:
             sorted_list.append(first_half.pop(0))
         else:
             sorted_list.append(second_half.pop(0))
@@ -25,5 +25,5 @@ def merge_sort(nums: List[int]) -> List[int]:
         sorted_list.extend(first_half)
     elif second_half:
         sorted_list.extend(second_half)
-    
+        
     return sorted_list
