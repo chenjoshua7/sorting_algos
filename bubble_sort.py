a059c02cce9db34bbfe3c569dc0d77cf81fa56d5
Joshua Chen <chen.joshua98@gmail.com>
2024-10-30 14:25:29 -0400

    completed bubble sort

--- bubble_sort.py
+++ bubble_sort.py
@@ -5,12 +5,14 @@ def bubble_sort(nums: List[int]) -> List[int]:
     n = len(nums_)
     count = 0
     
-    while count:
+    while True:
         count = 0
         for i in range(n-1):
             if nums_[i] > nums_[i+1]:
                 nums_[i], nums_[i+1] = nums_[i+1], nums_[i]
                 count += 1
+        if count == 0:
+            break
         
     return nums_
     
